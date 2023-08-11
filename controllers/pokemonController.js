27c0fc6a6548bbad4b2ebfee18a7d1a7345fc792
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-08-11 01:52:47 -0600

    added .img to data without modification inside the data file

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,13 +1,22 @@
 const pokemonData = require("../models/pokemonData"); // Here you are requiring the pokemonData module using require("../models/pokemonData");, which means you are importing the data from the pokemonData.js file as the pokemonData variable. Therefore, you should use pokemonData in your route handlers to reference the imported array of Pokemon data.
 
 module.exports.index = (req, res) => {
-  res.render("Index", { pokemonData });
+  const pokemonWithImgExtention = pokemonData.map((pokemon) => ({
+    ...pokemon,
+    img: ` ${pokemon.img}.jpg`,
+  }));
+  res.render("Index", { pokemonData: pokemonWithImgExtention });
 };
 
 module.exports.show = (req, res) => {
   let pokes = pokemonData[req.params.indexOfPokemon];
   if (pokes) {
-    res.render("Show", { pokemonData: pokes });
+    const pokemonWithImgExtention = {
+      ...pokes,
+      img: `${pokes.img}.jpg`,
+    };
+
+    res.render("Show", { pokemonData: pokemonWithImgExtention });
   } else {
     res.render("Error");
   }
